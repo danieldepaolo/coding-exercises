2252607ced525e1fbb7308aa162ab240295643ae
Daniel DePaolo <djdep13@gmail.com>
2018-09-15 18:11:16 -0700

    Add swipe capability for mobile.

--- zillow-frontend-exercise/src/components/PhotoGallery.js
+++ zillow-frontend-exercise/src/components/PhotoGallery.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Swipe from 'react-easy-swipe';
 
 import PhotoItem from './PhotoItem';
 import SwitchButton from './SwitchButton';
@@ -19,7 +20,7 @@ class PhotoGallery extends Component {
     };
   }
 
-  onPrevClicked = e => {
+  onPrevClicked = () => {
     if (this.state.currentPhoto > 0) {
       this.setState(prevState => {
         return {currentPhoto: prevState.currentPhoto - 1};
@@ -27,12 +28,20 @@ class PhotoGallery extends Component {
     }
   }
 
-  onNextClicked = e => {
+  onNextClicked = () => {
     if (this.state.currentPhoto < this.numPhotos - 1) {
       this.setState(prevState => {
         return {currentPhoto: prevState.currentPhoto + 1};
       });
     }
+  }
+
+  onSwipeRight = (e) => {
+    this.onPrevClicked();
+  }
+ 
+  onSwipeLeft = (e) => {
+    this.onNextClicked();
   }
 
   render() {
@@ -43,15 +52,20 @@ class PhotoGallery extends Component {
         <div className="btnColumn">
           <SwitchButton
             direction="prev"
-            clicked={e => this.onPrevClicked(e)}
+            clicked={e => this.onPrevClicked()}
             disabled={this.state.currentPhoto === 0}
           />
         </div>
-        <PhotoItem photo={photoData[this.state.currentPhoto]} />
+        <Swipe
+          onSwipeLeft={this.onSwipeLeft}
+          onSwipeRight={this.onSwipeRight}
+        >
+          <PhotoItem photo={photoData[this.state.currentPhoto]} />
+        </Swipe>
         <div className="btnColumn">
           <SwitchButton
             direction="next"
-            clicked={e => this.onNextClicked(e)}
+            clicked={e => this.onNextClicked()}
             disabled={this.state.currentPhoto === this.numPhotos - 1}
           />
         </div>
